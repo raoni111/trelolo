518662e3b5d571f4e86e385435a7ff48e53bb917
Raoni Oliveira Quevedo <Raoni.itar@gmail.com>
2025-04-30 10:01:07 -0300

    create-and-config-app

---
+++ src/components/Header-component.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="bg-default-background-color">
+    <h1 class="p-3 text-2xl text-white font-bold">Trelolo</h1>
+  </header>
+</template>
---
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -0,0 +1,6 @@
+import { describe, it, expect } from 'vitest'
+
+describe('HelloWorld', () => {
+  it('renders properly', () => {
+  })
+})
